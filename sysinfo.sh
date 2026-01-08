ae4541a05825b001279c6dead83dd8a9a944a5d3
Gokul K <K.Gokul@amd.com>
2026-01-08 16:08:17 +0530

    Updated sysinfo script

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,26 +1,58 @@
 #!/bin/bash
 
 if ! command -v sshpass &> /dev/null; then
-        echo "sshpass not found. Attempting to install..."
+    echo "sshpass not found. Attempting to install..."
 fi
 
-#Instead of volcano9f8e-os name directly using its ip
-hosts=("volcano9b5e-host" "volcano9dee-hostos" "volcano9f6e-hostos" "volcano99fc-os" "volcano9b0c-os" "10.86.20.188")
+hosts=(
+"volcano9b5e-host" "volcano9b5e-hostos"
+"volcano9dee-hostos" "volcano9dee-host"
+"volcano9f6e-hostos" "volcano99fc-os"
+"volcano99fc-hostos" "volcano99fc-host"
+"volcano9b0c-os" "volcano9b0c-hostos"
+"10.86.20.188"
+)
 
-echo > ~/.ssh/known_hosts
-rm -f /tmp/sysinfo_errlog
-echo "-----------------------------------"
-for host in "${hosts[@]}"; do
-    echo "Checking $host..."
-    sshpass -p 'Amd$1234!' ssh -o StrictHostKeyChecking=no  "amd@$host" bash 2>>/tmp/sysinfo_errlog << 'EOF'
-        cat /etc/os-release | grep "PRETTY" | cut -d= -f2 | xargs echo "#Installed OS -- > $1" ;
-        cat /etc/motd | xargs echo "#$1" ;
-        ip -4 -o addr show | awk '{print "IP addr:", $4}' | cut -d/ -f1 | grep "10.86"
-        echo "OS available in other disks:" ;
-        lsblk -o NAME | grep -e ^nvm -e home
-EOF
+working_host=()
+offline_host=()
+
+echo "Checking Host online status..."
+
+: > ~/.ssh/known_hosts
+rm -f /tmp/sysinfo_errlog /tmp/sysinfo_offline
+
+#Function to get infoo from online hosts
+check_host_info() {
     echo "-----------------------------------"
+    echo "Checking $1..."
+
+    sshpass -p 'Amd$1234!' ssh -o StrictHostKeyChecking=no "amd@$1" bash 2>>/tmp/sysinfo_errlog << 'EOF'
+cat /etc/os-release | grep "PRETTY" | cut -d= -f2 | xargs echo "#Installed OS -->"
+cat /etc/motd | xargs echo "#"
+ip -4 -o addr show | awk '{print "IP addr:", $4}' | cut -d/ -f1 | grep "10.86"
+echo "OS available in other disks:"
+lsblk -o NAME | grep -e ^nvm -e home
+EOF
+
+    echo "-----------------------------------"
+}
+
+i=0
+for host in "${hosts[@]}"; do
+    if sshpass -p 'Amd$1234!' ssh -o StrictHostKeyChecking=no "amd@$host" ls \
+        		>> /tmp/sysinfo_offline 2>> /tmp/sysinfo_errlog; then
+        check_host_info "$host"
+
+    else
+        if grep -qi "no route" /tmp/sysinfo_errlog; then
+            echo "-----------------------------------"
+            echo "$host is offline"
+            echo "-----------------------------------"
+        fi
+    fi
+    ((i++))
 done
 
 echo "#Source code @ /home/amd/.acp/sysinfo.sh"
 echo "#errors @ /tmp/sysinfo_errlog"
+
